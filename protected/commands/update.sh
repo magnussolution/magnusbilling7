c37b170abdf5c658168d110953ef57678723b150
info@magnusbilling.com <info@magnusbilling.com>
2017-08-09 15:20:46 -0300

    change update method to git pull

--- protected/commands/update.sh
+++ protected/commands/update.sh
@@ -3,16 +3,16 @@ clear
 echo
 echo
 echo
-echo "=======================WWW.MAGNUSBILLING.COM==========================";
-echo "_      _                               ______ _ _ _ _  			 ";
-echo "|\    /|                               | ___ (_) | (_) 			 ";
+echo "=======================WWW.MAGNUSBILLING.COM===========================";
+echo "_      _                               ______ _ _ _ _  			     ";
+echo "|\    /|                               | ___ (_) | (_) 			     ";
 echo "| \  / | ___  ____ _ __  _   _   _____ | |_/ /_| | |_ _ __   __ _ 	 ";
 echo "|  \/  |/   \/  _ \| '_ \| | | \| ___| | ___ \ | | | | '_ \ /  _ \	 ";
 echo "| |\/| |  | |  (_| | | | | |_| ||____  | |_/ / | | | | | | |  (_| |	 ";
 echo "|_|  |_|\___|\___  |_| | |_____|_____|  \___/|_|_|_|_|_| |_|\___  |	 ";
 echo "                _/ |                                           _/ |	 ";
 echo "               |__/                                           |__/ 	 ";
-echo "														 ";
+echo "														                 ";
 echo "============================== UPDATE =================================";
 echo
 
@@ -22,15 +22,19 @@ if [[ -e /var/www/html/mbilling/protected/commands/update2.sh ]]; then
 	/var/www/html/mbilling/protected/commands/update2.sh
 	exit;
 fi
-rm -rf /var/www/html/mbilling/MBilling*.tar.gz
+
 cd /var/www/html/mbilling
-wget http://master.dl.sourceforge.net/project/magnusbilling/MBilling-6-beta.tar.gz
-cd /var/www/html/mbilling
-rm -rf /var/www/html/mbilling/yii
-tar xzf MBilling-6-beta.tar.gz
-rm -rf /var/www/html/mbilling/MBilling*.tar.gz
+## pull remote git repository
+git pull
+
+##update database
 php /var/www/html/mbilling/cron.php UpdateMysql
+
+## remove unnecessary directories
 rm -rf /var/www/html/mbilling/doc
+rm -rf /var/www/html/mbilling/script
+
+## set default permissions 
 chown -R asterisk:asterisk /var/lib/php/session/
 chown -R asterisk:asterisk /var/spool/asterisk/outgoing/
 chown -R asterisk:asterisk /etc/asterisk
