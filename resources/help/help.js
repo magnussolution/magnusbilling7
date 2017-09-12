ef13edbe556821b791afc069f57906603c05ef35
info@magnusbilling.com <info@magnusbilling.com>
2017-09-12 10:34:49 -0300

    fix issue #31

--- resources/help/help.js
+++ resources/help/help.js
@@ -1,15 +1,15 @@
 Help.load({
-	'trunk.transport': 'Enable TLS transport. Need install certificate on Asterisk',
-	'trunk.encryption': 'Whether to offer SRTP encrypted media (ONLY TO SRTP)',
-	'trunk.port': 'Only port 5060 is open on IPTABLES.',
-	'sip.calllimit': 'Use 0 to unlimit',
-	'user.prefix_local' : 'It is used to change the dialing number.',
-	'user.typepaid' : 'To postpaid is necessary add credit limit.',
-	'trunk.user' : 'Only use if authentication is via user and pass.',
-	'trunk.secret' : 'Only use if authentication is via user and pass.',
-	'trunk.trunkprefix' : 'Add a prefix to send to your trunk',
-	'trunk.removeprefix' : 'Remove a prefix to send to your trunk',
-	'trunk.allow' : 'G723 and G729 are not installed',
-	'trunk.providertech' : 'You need install appropriate drive to use card like dgv extra Dongle',
-	'user.disk_space' : 'Insert the space available to record in GB. Use -1 to unlimit'
+    'trunk.transport': 'Enable TLS transport. Need install certificate on Asterisk',
+    'trunk.encryption': 'Whether to offer SRTP encrypted media (ONLY TO SRTP)',
+    'trunk.port': 'Only port 5060 is open on IPTABLES.',
+    'sip.calllimit': 'Use 0 to unlimit',
+    'user.prefix_local': 'It is used to change the dialing number.',
+    'user.typepaid': 'To postpaid is necessary add credit limit.',
+    'trunk.user': 'Only use if authentication is via user and pass.',
+    'trunk.secret': 'Only use if authentication is via user and pass.',
+    'trunk.trunkprefix': 'Add a prefix to send to your trunk',
+    'trunk.removeprefix': 'Remove a prefix to send to your trunk',
+    'trunk.allow': 'G723 and G729 are not installed',
+    'trunk.providertech': 'You need install appropriate drive to use card like dgv extra Dongle',
+    'user.disk_space': 'Insert the space available to record in GB. Use -1 to unlimit'
 });
