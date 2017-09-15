0426db4e1ba69c99c41a8b89b2039af6e50d6865
info@magnusbilling.com <info@magnusbilling.com>
2017-09-15 16:56:38 -0300

    fix paghiper payment method

--- protected/controllers/PagHiperController.php
+++ protected/controllers/PagHiperController.php
@@ -1,8 +1,24 @@
 <?php
 
 /**
- * Url for moip ruturn http://ip/billing/index.php/pagSeguro .
- * https://pagseguro.uol.com.br/preferences/automaticReturn.jhtml
+ * Acoes do modulo "Methodpay".
+ *
+ * =======================================
+ * ###################################
+ * MagnusBilling
+ *
+ * @package MagnusBilling
+ * @author Adilson Leffa Magnus.
+ * @copyright Copyright (C) 2005 - 2016 MagnusBilling. All rights reserved.
+ * ###################################
+ *
+ * This software is released under the terms of the GNU Lesser General Public License v2.1
+ * A copy of which is available from http://www.gnu.org/copyleft/lesser.html
+ *
+ * Please submit bug reports, patches, etc to https://github.com/magnusbilling/mbilling/issues
+ * =======================================
+ * Magnusbilling.com <info@magnusbilling.com>
+ * 04/09/2017
  */
 class PagHiperController extends Controller
 {
