da80ed273bcc4924f3fce1445f008049ed1ae01b
info@magnusbilling.com <info@magnusbilling.com>
2018-05-29 11:57:32 -0300

    call failed ORDER BY t.id

--- protected/controllers/CallFailedController.php
+++ protected/controllers/CallFailedController.php
@@ -22,8 +22,7 @@
 
 class CallFailedController extends Controller
 {
-    public $attributeOrder = 'starttime DESC';
-
+    public $attributeOrder = 'id DESC';
     public $extraValues    = array(
         'idUser'   => 'username',
         'idPlan'   => 'name',
