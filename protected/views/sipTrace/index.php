213b09ffb7e2b03f12e96d80422a860e3ea8acba
info@magnusbilling.com <info@magnusbilling.com>
2019-01-30 09:49:45 -0300

    avoid insecure issue

--- protected/views/sipTrace/index.php
+++ protected/views/sipTrace/index.php
@@ -1,18 +1,85 @@
-<table border=1 width="100%">
+<style type="text/css">
+
+#border {
+border: 5px solid #1C6EA4;
+border-radius: 9px;
+}
+
+table.blueTable {
+  border: 1px solid #1C6EA4;
+
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.blueTable td, table.blueTable th {
+  border: 1px solid #AAAAAA;
+  padding: 3px 2px;
+}
+table.blueTable tbody td {
+  font-size: 13px;
+}
+table.blueTable tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.blueTable thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table.blueTable thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table.blueTable thead th:first-child {
+  border-left: none;
+}
+
+table.blueTable tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFFFFF;
+  background: #D0E4F5;
+  background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  border-top: 2px solid #444444;
+}
+table.blueTable tfoot td {
+  font-size: 14px;
+}
+table.blueTable tfoot .links {
+  text-align: right;
+}
+table.blueTable tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
+
+</style>
+<div id="border">
+<table class="blueTable" width="100%">
 	<tr>
 		<td width="50%" valign="top">
-			<table border=1 width="100%">
+			<table class="blueTable" width="100%">
 				<tr>
 					<td width="30%"></td>
-					<td width="20%"><u><?php echo $packet[0]['fromip'] ?></u></td>
+					<td width="20%"><u><b><?php echo $packet[0]['fromip'] ?></b></u></td>
 					<td width="30%"></td>
-					<td width="20%"><u><?php echo $packet[0]['toip'] ?></u></td>
+					<td width="20%"><u><b><?php echo $packet[0]['toip'] ?></b></u></td>
 				</tr>
 				<?php foreach ($packet as $key => $value): ?>
 				<tr>
 					<td><?php echo $value['date'] ?></td>
-					<td></td>
-					<td>
+
+					<td colspan="3">
 						<div align="center" onclick='selectMethod("<?php echo $value['id'] ?>","<?php echo count($packet) ?>")' >
 							<font color="<?php echo $value['direction'] ?>">
 								<?php echo $value['method'] ?><br><?php echo $value['direction'] == 'red' ? '&rarr;' : '&larr;' ?>
@@ -20,7 +87,7 @@
 							</font>
 						</div>
 					</td>
-					<td></td>
+
 				</tr>
 				<?php endforeach;?>
 			</table>
@@ -32,7 +99,7 @@
 		</td>
 	</tr>
 </table>
-
+</div>
 
 <script type="text/javascript">
 
