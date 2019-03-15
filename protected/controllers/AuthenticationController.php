7812d1373c53aeee07f23ebd054ae55a8fb482db
info@magnusbilling.com <info@magnusbilling.com>
2019-03-15 14:59:30 -0300

    not request SFA if already logged from same IP

--- protected/controllers/AuthenticationController.php
+++ protected/controllers/AuthenticationController.php
@@ -141,6 +141,18 @@ class AuthenticationController extends Controller
 
             Yii::app()->session['googleAuthenticatorKey'] = $ga->getQRCodeGoogleUrl('VoIP-' . $modelUser->username . '-' . $modelUser->id, $secret);
 
+            $modelLogUsers = LogUsers::model()->count('id_user = :key AND ip = :key1 AND description = :key2',
+                array(
+                    ':key'  => $modelUser->id,
+                    ':key1' => $_SERVER['REMOTE_ADDR'],
+                    ':key2' => 'Username Login on the panel - User ' . $modelUser->username,
+                ));
+            if ($modelLogUsers > 0) {
+                Yii::app()->session['checkGoogleAuthenticator'] = false;
+            } else {
+                Yii::app()->session['checkGoogleAuthenticator'] = true;
+            }
+
             Yii::app()->session['checkGoogleAuthenticator'] = true;
         } else {
             Yii::app()->session['showGoogleCode']           = false;
