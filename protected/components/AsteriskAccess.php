263720d769126d3a74a9d3089a93cc06daad1c23
info@magnusbilling.com <info@magnusbilling.com>
2020-05-22 11:28:01 -0300

    fix callerid issue

--- protected/components/AsteriskAccess.php
+++ protected/components/AsteriskAccess.php
@@ -734,7 +734,7 @@ class AsteriskAccess
 
                     if (strlen($sip->cid_number) > 1) {
                         $line .= 'cid_number=' . $sip->cid_number . "\n";
-                        $line .= 'callerid=' . $iax->cid_number . "\n";
+                        $line .= 'callerid=' . $sip->cid_number . "\n";
                     }
 
                     if (strlen($sip->language) > 1) {
