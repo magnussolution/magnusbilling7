4fdc90fddc0209c6295627705d9f7aa931919857
info@magnusbilling.com <info@magnusbilling.com>
2020-07-03 17:26:45 -0300

    build

--- protected/commands/UpdateMysqlCommand.php
+++ protected/commands/UpdateMysqlCommand.php
@@ -750,11 +750,11 @@ exten => s,1,Set(MASTER_CHANNEL(TRUNKANSWERTIME)=\${EPOCH})
         }
 
         //2020-06-15
-        if ($version == '7.3.1') {
+        if ($version == '7.3.0') {
             $sql = " ALTER TABLE `pkg_rate_provider` ADD INDEX(`id_prefix`);";
             $this->executeDB($sql);
 
-            $version = '7.3.0';
+            $version = '7.3.1';
             $sql     = "UPDATE pkg_configuration SET config_value = '" . $version . "' WHERE config_key = 'version' ";
             Yii::app()->db->createCommand($sql)->execute();
         }
