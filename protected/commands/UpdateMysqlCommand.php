5aca9e87caaef1f5029411534df64a7724c0a549
info@magnusbilling.com <info@magnusbilling.com>
2020-10-05 16:06:24 -0300

    increase payment method token field

--- protected/commands/UpdateMysqlCommand.php
+++ protected/commands/UpdateMysqlCommand.php
@@ -1133,6 +1133,17 @@ exten => s,1,Set(MASTER_CHANNEL(TRUNKANSWERTIME)=\${EPOCH})
             Yii::app()->db->createCommand($sql)->execute();
         }
 
+        //2020-10-05
+        if ($version == '7.5.1') {
+
+            $sql = " ALTER TABLE `pkg_method_pay` CHANGE `pagseguro_TOKEN` `pagseguro_TOKEN` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL; ";
+            $this->executeDB($sql);
+
+            $version = '7.5.2';
+            $sql     = "UPDATE pkg_configuration SET config_value = '" . $version . "' WHERE config_key = 'version' ";
+            Yii::app()->db->createCommand($sql)->execute();
+        }
+
     }
 
     public function executeDB($sql)
