1c15ae7d706cf3f5536dbf84d7e48f9e05dfa5ba
MagnusSolution <adilsom81@yahoo.com.br>
2020-10-15 19:14:17 -0300

    Update pt_BR.js

--- resources/locale/pt_BR.js
+++ resources/locale/pt_BR.js
@@ -14,7 +14,7 @@
  * 2020-08-07
  */
 Locale.load({
-    'Company website': 'Site da empresa',
+    'Company website': 'Site da empresasjsjte',
     'MagnusBilling System': 'Sistema MagnusBilling',
     'Activate offer': 'Ativar pacote grátis',
     'Voucher inexistente or already used': 'Voucher inexistente ou já usado',
