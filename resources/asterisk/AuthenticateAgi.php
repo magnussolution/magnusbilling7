9b7075469ff9937c9ea015893c84a8666d63589a
magnussolution <info@magnusbilling.com>
2022-04-12 17:11:03 -0300

    user call limit only UP or Ring calls

--- resources/asterisk/AuthenticateAgi.php
+++ resources/asterisk/AuthenticateAgi.php
@@ -392,7 +392,7 @@ class AuthenticateAgi
             $sipAccounts = substr($sipAccounts, 0, -1);
             $calls       = 0;
             foreach ($channelsData as $key => $line) {
-                if (preg_match("/^SIP\/($sipAccounts)-/", $line)) {
+                if (preg_match("/^SIP\/($sipAccounts)-.*(Ring|Up)/", $line)) {
                     $calls++;
                 }
             }
