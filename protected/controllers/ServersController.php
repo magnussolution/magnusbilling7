0bb40d31b5dba7008dc71edac32ab4d4752b4a5a
magnussolution <info@magnusbilling.com>
2022-09-26 13:01:56 -0300

    improve servers synchronization with proxy

--- protected/controllers/ServersController.php
+++ protected/controllers/ServersController.php
@@ -62,10 +62,21 @@ class ServersController extends Controller
                         AND status = 1 AND weight > 0");
 
             foreach ($modelServerAS as $key => $server) {
-                $sql = "INSERT INTO $dbname.$table (setid,destination,weight,description) VALUES ('1','sip:" . $server->host . ":" . $server->sip_port . "','" . $server->weight . "','" . $server->description . "')";
-                $con->createCommand($sql)->execute();
+
+                if ($this->ip_is_private($hostname)) {
+                    $sql = "INSERT INTO $dbname.$table (setid,destination,weight,description) VALUES ('1','sip:" . $server->host . ":" . $server->sip_port . "','" . $server->weight . "','" . $server->description . "')";
+                } else {
+                    $sql = "INSERT INTO $dbname.$table (setid,destination,weight,description) VALUES ('1','sip:" . $server->public_ip . ":" . $server->sip_port . "','" . $server->weight . "','" . $server->description . "')";
+                }
+
+                try {
+                    $con->createCommand($sql)->execute();
+                } catch (Exception $e) {
+                    return;
+                }
 
             }
+
         }
 
         $this->generateSipFile();
@@ -99,6 +110,32 @@ class ServersController extends Controller
     public function afterDestroy($values)
     {
         $this->generateSipFile();
+    }
+
+    public function ip_is_private($ip)
+    {
+        $pri_addrs = array(
+            '10.0.0.0|10.255.255.255', // single class A network
+            '172.16.0.0|172.31.255.255', // 16 contiguous class B network
+            '192.168.0.0|192.168.255.255', // 256 contiguous class C network
+            '169.254.0.0|169.254.255.255', // Link-local address also refered to as Automatic Private IP Addressing
+            '127.0.0.0|127.255.255.255', // localhost
+        );
+
+        $long_ip = ip2long($ip);
+        if ($long_ip != -1) {
+
+            foreach ($pri_addrs as $pri_addr) {
+                list($start, $end) = explode('|', $pri_addr);
+
+                // IF IS PRIVATE
+                if ($long_ip >= ip2long($start) && $long_ip <= ip2long($end)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
