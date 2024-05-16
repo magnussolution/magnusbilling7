feea7f4fc11e1db74dc509b76603e83abee1d990
magnus <magnusadilson@gmail.com>
2024-05-16 14:40:18 -0300

    fix google token qrcode

--- lib/GoogleAuthenticator/GoogleAuthenticator.php
+++ lib/GoogleAuthenticator/GoogleAuthenticator.php
@@ -48,7 +48,7 @@ class PHPGangsta_GoogleAuthenticator
         $secretkey = $this->_base32Decode($secret);
 
         // Pack time into binary string
-        $time = chr(0).chr(0).chr(0).chr(0).pack('N*', $timeSlice);
+        $time = chr(0) . chr(0) . chr(0) . chr(0) . pack('N*', $timeSlice);
         // Hash it with users secret key
         $hm = hash_hmac('SHA1', $time, $secretkey, true);
         // Use last nipple of result as index/offset
@@ -74,12 +74,13 @@ class PHPGangsta_GoogleAuthenticator
      * @param string $title
      * @return string
      */
-    public function getQRCodeGoogleUrl($name, $secret, $title = null) {
-        $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
-	if(isset($title)) {
-                $urlencoded .= urlencode('&issuer='.urlencode($title));
+    public function getQRCodeGoogleUrl($name, $secret, $title = null)
+    {
+        $urlencoded = urlencode('otpauth://totp/' . $name . '?secret=' . $secret . '');
+        if (isset($title)) {
+            $urlencoded .= urlencode('&issuer=' . urlencode($title));
         }
-        return 'https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl='.$urlencoded.'';
+        return 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' . $urlencoded . '';
     }
 
     /**
@@ -99,7 +100,7 @@ class PHPGangsta_GoogleAuthenticator
 
         for ($i = -$discrepancy; $i <= $discrepancy; $i++) {
             $calculatedCode = $this->getCode($secret, $currentTimeSlice + $i);
-            if ($calculatedCode == $code ) {
+            if ($calculatedCode == $code) {
                 return true;
             }
         }
@@ -127,30 +128,41 @@ class PHPGangsta_GoogleAuthenticator
      */
     protected function _base32Decode($secret)
     {
-        if (empty($secret)) return '';
+        if (empty($secret)) {
+            return '';
+        }
 
-        $base32chars = $this->_getBase32LookupTable();
+        $base32chars        = $this->_getBase32LookupTable();
         $base32charsFlipped = array_flip($base32chars);
 
         $paddingCharCount = substr_count($secret, $base32chars[32]);
-        $allowedValues = array(6, 4, 3, 1, 0);
-        if (!in_array($paddingCharCount, $allowedValues)) return false;
-        for ($i = 0; $i < 4; $i++){
-            if ($paddingCharCount == $allowedValues[$i] &&
-                substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) return false;
+        $allowedValues    = [6, 4, 3, 1, 0];
+        if ( ! in_array($paddingCharCount, $allowedValues)) {
+            return false;
         }
-        $secret = str_replace('=','', $secret);
-        $secret = str_split($secret);
+
+        for ($i = 0; $i < 4; $i++) {
+            if ($paddingCharCount == $allowedValues[$i] &&
+                substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) {
+                return false;
+            }
+
+        }
+        $secret       = str_replace('=', '', $secret);
+        $secret       = str_split($secret);
         $binaryString = "";
-        for ($i = 0; $i < count($secret); $i = $i+8) {
+        for ($i = 0; $i < count($secret); $i = $i + 8) {
             $x = "";
-            if (!in_array($secret[$i], $base32chars)) return false;
+            if ( ! in_array($secret[$i], $base32chars)) {
+                return false;
+            }
+
             for ($j = 0; $j < 8; $j++) {
                 $x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);
             }
             $eightBits = str_split($x, 8);
             for ($z = 0; $z < count($eightBits); $z++) {
-                $binaryString .= ( ($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48 ) ? $y:"";
+                $binaryString .= (($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48) ? $y : "";
             }
         }
         return $binaryString;
@@ -165,27 +177,35 @@ class PHPGangsta_GoogleAuthenticator
      */
     protected function _base32Encode($secret, $padding = true)
     {
-        if (empty($secret)) return '';
+        if (empty($secret)) {
+            return '';
+        }
 
         $base32chars = $this->_getBase32LookupTable();
 
-        $secret = str_split($secret);
+        $secret       = str_split($secret);
         $binaryString = "";
         for ($i = 0; $i < count($secret); $i++) {
             $binaryString .= str_pad(base_convert(ord($secret[$i]), 10, 2), 8, '0', STR_PAD_LEFT);
         }
         $fiveBitBinaryArray = str_split($binaryString, 5);
-        $base32 = "";
-        $i = 0;
+        $base32             = "";
+        $i                  = 0;
         while ($i < count($fiveBitBinaryArray)) {
             $base32 .= $base32chars[base_convert(str_pad($fiveBitBinaryArray[$i], 5, '0'), 2, 10)];
             $i++;
         }
         if ($padding && ($x = strlen($binaryString) % 40) != 0) {
-            if ($x == 8) $base32 .= str_repeat($base32chars[32], 6);
-            elseif ($x == 16) $base32 .= str_repeat($base32chars[32], 4);
-            elseif ($x == 24) $base32 .= str_repeat($base32chars[32], 3);
-            elseif ($x == 32) $base32 .= $base32chars[32];
+            if ($x == 8) {
+                $base32 .= str_repeat($base32chars[32], 6);
+            } elseif ($x == 16) {
+                $base32 .= str_repeat($base32chars[32], 4);
+            } elseif ($x == 24) {
+                $base32 .= str_repeat($base32chars[32], 3);
+            } elseif ($x == 32) {
+                $base32 .= $base32chars[32];
+            }
+
         }
         return $base32;
     }
@@ -197,12 +217,12 @@ class PHPGangsta_GoogleAuthenticator
      */
     protected function _getBase32LookupTable()
     {
-        return array(
+        return [
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', //  7
             'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', // 15
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', // 23
             'Y', 'Z', '2', '3', '4', '5', '6', '7', // 31
-            '='  // padding char
-        );
+            '=', // padding char
+        ];
     }
 }
